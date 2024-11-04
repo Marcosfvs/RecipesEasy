70b3c100e9497fbf1329dafea1c5700293d20e60
Marcos Silva <marcos.fvsilva11@hotmail.com>
2024-11-04 21:41:48 +0000

    Start recipes app

--- app/src/main/java/com/devspace/myapplication/MainActivity.kt
+++ app/src/main/java/com/devspace/myapplication/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
@@ -17,6 +21,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.devspace.myapplication.ui.theme.EasyRecipesTheme
 import retrofit2.Call
@@ -38,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     val apiService = RetrofitClient.retrofitInstance.create(ApiService::class.java)
                     val callRandomRecipes = apiService.getRandomRecipes()
 
-                    callRandomRecipes.enqueue(object : Callback<RecipesResponse>{
+                    callRandomRecipes.enqueue(object : Callback<RecipesResponse> {
                         override fun onResponse(
                             call: Call<RecipesResponse>,
                             response: Response<RecipesResponse>
@@ -57,8 +67,12 @@ class MainActivity : ComponentActivity() {
 
                     })
 
-                        RecipesList(
-                            recipesList = randomRecipes)
+                    RecipesList(
+                        recipesList = randomRecipes,
+
+                    ){ itemCliked ->
+
+                    }
 
 
                 }
@@ -70,12 +84,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun RecipesList(
-    recipesList: List<RecipeDTO>
+    recipesList: List<RecipeDTO>,
+    onClick: (RecipeDTO) -> Unit
 ) {
     LazyColumn {
         items(recipesList) {
             RecipesItem(
                 recipesDTO = it,
+                onClick = onClick
             )
         }
     }
@@ -85,10 +101,33 @@ fun RecipesList(
 @Composable
 fun RecipesItem(
     recipesDTO: RecipeDTO,
+    onClick: (RecipeDTO) -> Unit
 ) {
-    Column {
-        Text(text = recipesDTO.title)
-        AsyncImage(model = recipesDTO.image, contentDescription = "Recipe Image")
-        //Text(text = recipesDTO.summary)
+    Column(modifier = Modifier
+        .clickable {
+            onClick.invoke(recipesDTO)
+        }
+    ) {
+        Text(
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 24.sp,
+            text = recipesDTO.title
+        )
+        AsyncImage(
+            modifier = Modifier
+                .padding(all = 4.dp)
+                .width(120.dp)
+                .height(150.dp),
+            contentScale = ContentScale.Crop,
+            model = recipesDTO.image,
+            contentDescription = "${recipesDTO.title}Recipe Image"
+        )
+        Text(
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            text = recipesDTO.summary
+        )
     }
 }
